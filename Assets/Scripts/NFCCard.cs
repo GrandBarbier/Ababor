67bc8d6c399914b615eef72ea37c78945eac7ffe
Drimnas <clovismichaud1287@gmail.com>
2021-12-16 09:41:55 +0100

    Resolve build bug

---
+++ Assets/Scripts/NFCCard.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Wizama.Hardware.Antenna;
+
+public class NFCCard : MonoBehaviour
+{
+    void Start()  {  
+        NFCController.StartPolling( NFC_DEVICE_ID.ANTENNA_1, NFC_DEVICE_ID.ANTENNA_8, NFC_DEVICE_ID.ANTENNA_14, NFC_DEVICE_ID.ANTENNA_21);  
+    }   
+    
+    void OnDisable()  {  
+        NFCController.StopPolling();  
+    }   
+    
+    void FixedUpdate()  {  
+        List<NFCTag> antenna1Tags = NFCController.GetTags(NFC_DEVICE_ID.ANTENNA_1);   
+        foreach (NFCTag tag in antenna1Tags)  
+            Debug.Log(tag.Data + " placed on antenna 1");   
+        
+        List<NFCTag>[] allAntennaTags = NFCController.GetTags();   
+        int count = 0;  
+        foreach (List<NFCTag> Tags in allAntennaTags)  
+            count += Tags.Count;   
+            
+        Debug.Log("There is " + count + " tags detected in total");  
+    } 
+}
