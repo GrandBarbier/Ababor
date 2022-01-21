3796905f7907c1dc57c24be63c07480f1c18e3f9
Lelio <lelio.rollin@e-artsup.net>
2022-01-21 15:08:54 +0100

    Cam

--- Assets/Scripts/CamControler.cs
+++ Assets/Scripts/CamControler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,19 @@ using UnityEngine;
 public class CamControler : MonoBehaviour
 {
     public Camera cam;
-    public Transform pivot;
 
-    public float panSpeed, smoothSpeed;
+    [SerializeField]
+    private float panSpeed, smoothSpeed, zoomSpeed, minLimit, maxLimit, targetOffset, bounceSpeed;
+    
+    [SerializeField] 
+    private bool posSaved, moving, bouncing;
+
+    [SerializeField] 
+    private Vector3 savedPos, minPos, maxPos;
+    
+    private Vector3 panPos0, panPos1, panPos2, panPos3;
+
+    public LayerMask mask, panMask;
     
     // Start is called before the first frame update
     void Start()
@@ -19,20 +30,32 @@ public class CamControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount == 1)
-        {
-           PanTranslate();
-        }
-        else if (Input.touchCount >= 2)
-        {
-            var pos1  = Input.GetTouch(0).position;
-            var pos2  = Input.GetTouch(1).position; 
-            var pos1b = Input.GetTouch(0).deltaPosition;
-            var pos2b = Input.GetTouch(1).deltaPosition;
+        SetZoomLimits();
+        SetPanLimits();
 
-            var zoom = pos1b / pos2b;
-            Debug.Log(zoom);
-//            cam.transform.position += cam.transform.rotation * new Vector3(0,0, );
+        if (!moving)
+        {
+            if (Input.touchCount == 1)
+            {
+                PanTranslate();
+            }
+            else if (Input.touchCount >= 2 && !bouncing)
+            {
+                ZoomTranslate();
+            }
+            
+            if (Input.touchCount < 2)
+            {
+                if (posSaved)
+                {
+                    GoToSavedPos();
+                }
+                if (Vector3.Distance(cam.transform.position, savedPos) < targetOffset)
+                {
+                    posSaved = false;
+                    bouncing = false;
+                }
+            }
         }
     }
 
@@ -40,8 +63,118 @@ public class CamControler : MonoBehaviour
     {
         if (Input.GetTouch(0).phase == TouchPhase.Moved)
         {
-            Vector3 delta = cam.transform.position + new Vector3(Input.GetTouch(0).deltaPosition.x,0,Input.GetTouch(0).deltaPosition.y) * -panSpeed;
+            Vector3 delta = cam.transform.position + new Vector3(- Input.GetTouch(0).deltaPosition.x,0,- Input.GetTouch(0).deltaPosition.y) * panSpeed;
             cam.transform.position = Vector3.LerpUnclamped(cam.transform.position, delta, smoothSpeed);
         }
+    }
+
+    private void ZoomTranslate()
+    {
+        Touch touch1 = Input.GetTouch(0);
+        Touch touch2 = Input.GetTouch(1);
+            
+        Vector2 pos1  = touch1.position - touch1.deltaPosition;
+        Vector2 pos2  = touch2.position - touch2.deltaPosition;
+
+        float prevM = (pos1 - pos2).magnitude;
+        float currentM = (touch1.position - touch2.position).magnitude;
+
+        float diff = currentM - prevM;
+
+        Vector3 delta = cam.transform.position + cam.transform.rotation * new Vector3(0, 0, diff * zoomSpeed);
+        
+        if (Physics.Raycast(cam.transform.position, cam.transform.rotation * Vector3.forward, out RaycastHit hit, Mathf.Infinity, mask))
+        {
+            Debug.DrawLine(cam.transform.position, hit.point, Color.red);
+            cam.transform.position = Vector3.LerpUnclamped(cam.transform.position,hit.point , (diff * zoomSpeed / 100) * smoothSpeed * 0.1f);
+        }
+        else
+        {
+            cam.transform.position = Vector3.LerpUnclamped(cam.transform.position, delta, smoothSpeed * 0.1f);
+        }
+    }
+
+    private void SetZoomLimits()
+    {
+        if (Physics.Raycast(cam.transform.position, cam.transform.rotation * Vector3.forward, out RaycastHit hit, Mathf.Infinity, mask))
+        {
+            Vector3 dir = (cam.transform.position - hit.point).normalized;
+
+            minPos = hit.point + dir * minLimit;
+            maxPos = hit.point + dir * maxLimit;
+            
+            Debug.DrawLine(hit.point,  minPos, Color.red);
+            Debug.DrawLine(minPos, maxPos, Color.green);
+
+            if (Vector3.Distance(minPos, maxPos) < Vector3.Distance(minPos, cam.transform.position))
+            {
+                Debug.DrawLine(maxPos, cam.transform.position, Color.red);
+                SavePos(maxPos);
+            }
+            else if (Vector3.Distance(hit.point, minPos) > Vector3.Distance(hit.point, cam.transform.position))
+            {
+                SavePos(minPos);
+            }
+            else
+            {
+                posSaved = false;
+                bouncing = false;
+            }
+        }
+    }
+
+    private void SetPanLimits()
+    {
+        if (Physics.Raycast(cam.transform.position, Vector3.right, out RaycastHit hit0, Mathf.Infinity, panMask))
+        {
+            panPos0 = hit0.point;
+        }
+        else
+        {
+            Debug.DrawLine(cam.transform.position, panPos0, Color.red);
+            SavePos(panPos0);
+        }
+        
+        if (Physics.Raycast(cam.transform.position, Vector3.left, out RaycastHit hit1, Mathf.Infinity, panMask))
+        {
+            panPos1 = hit1.point;
+        }
+        else
+        {
+            Debug.DrawLine(cam.transform.position, panPos1, Color.red);
+            SavePos(panPos1);
+        }
+        
+        if (Physics.Raycast(cam.transform.position, Vector3.forward, out RaycastHit hit2, Mathf.Infinity, panMask))
+        {
+            panPos2 = hit2.point;
+        }
+        else
+        {
+            Debug.DrawLine(cam.transform.position, panPos2, Color.red);
+            SavePos(panPos2);
+        }
+        
+        if (Physics.Raycast(cam.transform.position, Vector3.back, out RaycastHit hit3, Mathf.Infinity, panMask))
+        {
+            panPos3 = hit3.point;
+        }
+        else
+        {
+            Debug.DrawLine(cam.transform.position, panPos3, Color.red);
+            SavePos(panPos3);
+        }
+    }
+    
+    private void SavePos(Vector3 pos)
+    {
+        savedPos = pos;
+        posSaved = true;
+    }
+    
+    private void GoToSavedPos()
+    {
+        bouncing = true;
+        cam.transform.position = Vector3.LerpUnclamped(cam.transform.position, savedPos, bounceSpeed);
     }
 }
