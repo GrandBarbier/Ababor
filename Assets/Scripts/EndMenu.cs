955495ad05d043a1519e8ead2479ca966a8fba4e
Amaury Janneteau <amaury.janneteau@e-artsup.net>
2022-04-27 10:05:00 +0200

    progress end menu script

--- Assets/Scripts/EndMenu.cs
+++ Assets/Scripts/EndMenu.cs
@@ -7,6 +7,8 @@ using UnityEngine.UI;
 
 public class EndMenu : MonoBehaviour
 {
+    public List<Player> allPlayers;
+
     private GameplayManager _gameplayManager;
     
     public List<TMP_Text> textPoint;
@@ -14,21 +16,27 @@ public class EndMenu : MonoBehaviour
     public GameObject downGameObject;
     public List<Image> backgrounds;
     public List<TMP_Text> textGoal;
+
+    public List<SpriteRenderer> placement = new List<SpriteRenderer>(6);
     
     public Objectif goal;
     
     public float step;
     public float scaleFactor;
 
-    private float alpha1;
-    private float alpha2;
-    private float alpha3;
+    private float alpha1 = 0f;
+    private float alpha2 = 0f;
+    private float alpha3 = 0f;
+    private float alphaTxt = 0f;
 
+
+    private bool once = true;
     public bool oui;
 
     private void Awake()
     {
         _gameplayManager = FindObjectOfType<GameplayManager>();
+        allPlayers = _gameplayManager.allPlayers;
     }
 
     // Start is called before the first frame update
@@ -36,10 +44,19 @@ public class EndMenu : MonoBehaviour
     {
         if (textPoint[0].text != _gameplayManager.allPlayers[0].point.ToString())
         {
-            textPoint[0].text = _gameplayManager.allPlayers[0].point.point.ToString();
-            textPoint[1].text = _gameplayManager.allPlayers[1].point.point.ToString();
-            textPoint[2].text = _gameplayManager.allPlayers[2].point.point.ToString();
-            textPoint[3].text = _gameplayManager.allPlayers[3].point.point.ToString();
+            textPoint[0].text = _gameplayManager.allPlayers[0].point.gold.ToString();
+            textPoint[1].text = _gameplayManager.allPlayers[1].point.gold.ToString();
+            textPoint[2].text = _gameplayManager.allPlayers[2].point.gold.ToString();
+            textPoint[3].text = _gameplayManager.allPlayers[3].point.gold.ToString();
+        }
+        if (textGoal[0].text != goal.descriptionsEarly[0])
+        {
+            textGoal[0].text = goal.descriptionsEarly[0];
+            textGoal[1].text = goal.descriptionsEarly[1];
+            textGoal[2].text = goal.descriptionMid[0];
+            textGoal[3].text = goal.descriptionMid[1];
+            textGoal[4].text = goal.descriptionLate[0];
+            textGoal[5].text = goal.descriptionLate[1];
         }
     }
 
@@ -51,23 +68,19 @@ public class EndMenu : MonoBehaviour
             MoveDown();
             ScaleDown();
         }
-        ShowObj();
-        
-    }
-    
-    public void ShowObj()
-    {
-        if (textGoal[0].text != goal.descriptionsEarly[0])
+        if (heads.transform.position == downGameObject.transform.position && alphaTxt < 1)
+            ShowObj();
+        if (once)
         {
-            textGoal[0].text = goal.descriptionsEarly[0];
-            textGoal[1].text = goal.descriptionsEarly[1];
-            textGoal[2].text = goal.descriptionMid[0];
-            textGoal[3].text = goal.descriptionMid[1];
-            textGoal[4].text = goal.descriptionLate[0];
-            textGoal[5].text = goal.descriptionLate[1];
+            ShowFacesOnObj();
+            AddToScore();
+            once = false;
         }
-        
-        if (alpha1 < 0.9 && heads.transform.position == downGameObject.transform.position)
+    }
+
+    private void ShowObj()
+    {
+        if (alpha1 < 0.9)
         {
             alpha1 += 0.02f;
             backgrounds[0].color = new Color(1, 1, 1, alpha1);
@@ -76,7 +89,7 @@ public class EndMenu : MonoBehaviour
             textGoal[1].color = new Color(1, 1, 1, alpha1);
 
         }
-        else if (alpha2 < 0.9 && heads.transform.position == downGameObject.transform.position)
+        else if (alpha2 < 0.9)
         {
             alpha2 += 0.02f;
             backgrounds[2].color = new Color(1, 1, 1, alpha2);
@@ -85,7 +98,7 @@ public class EndMenu : MonoBehaviour
             textGoal[3].color = new Color(1, 1, 1, alpha2);
 
         }
-        else if (alpha3 < 0.9 && heads.transform.position == downGameObject.transform.position)
+        else if (alpha3 < 0.9)
         {
             alpha3 += 0.02f;
             backgrounds[4].color = new Color(1, 1, 1, alpha3);
@@ -93,14 +106,43 @@ public class EndMenu : MonoBehaviour
             textGoal[4].color = new Color(1, 1, 1, alpha3);
             textGoal[5].color = new Color(1, 1, 1, alpha3);
         }
+        else if (alphaTxt < 1)
+        {
+            alphaTxt += 0.01f;
+            textGoal[6].color = new Color(textGoal[6].color.r, textGoal[6].color.g, textGoal[6].color.b, alpha3);
+            textGoal[7].color = new Color(textGoal[7].color.r, textGoal[7].color.g, textGoal[7].color.b, alpha3);
+            textGoal[8].color = new Color(textGoal[8].color.r, textGoal[8].color.g, textGoal[8].color.b, alpha3);
+
+        }
     }
 
-    public void MoveDown()
+    private void ShowFacesOnObj()
+    {
+        Debug.LogError("ouiiiiiiiiiiiii");
+        for (int i = 0; i < allPlayers.Count; i++)
+        {
+            for (int j = 0; j < allPlayers[i].point.objectifVerif.Count; j++)
+            {
+                if (allPlayers[i].point.objectifVerif[j])
+                {
+                    placement[j] = allPlayers[i].player.GetComponent<SpriteRenderer>();
+                    placement[j].gameObject.SetActive(true);
+                }
+            }
+        }
+    }
+
+    public void AddToScore()
+    {
+        
+    }
+
+    private void MoveDown()
     {
         heads.transform.position = Vector3.MoveTowards(heads.transform.position, downGameObject.transform.position, step);
     }
 
-    public void ScaleDown()
+    private void ScaleDown()
     {
         heads.transform.localScale -= new Vector3(scaleFactor, scaleFactor, 0);
     }
